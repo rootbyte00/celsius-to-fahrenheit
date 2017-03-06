2a3ff796cb5ad5185336c5ead0651c4bb42dec66
root <rootbyte00@gmail.com>
2017-03-07 01:16:32 +0200

    2 commit

---
+++ ftoc_v1.1.2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
+#sorry for this cancer i am a nood/new to this
+print ('Convert Celsius To Fahrenheit')
+celsius =  (int(input('Type A Number: ')))
+celsius = celsius * 9 / 5 + 32
+fahrenheit = ('°F')
+print ('Its {0}{1}'.format(celsius, fahrenheit))
